22b0ba80b0af10481b69f48914caa1a688fb5e60
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2025-11-24 20:52:29 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SPREADSHEET_ID = '1I_0c8p8r9Ep2VVQdFdeL0IsyTM7w18QGI-Hj40dMxt4';
 // 2. De Web App URL van je Google Apps Script
 //    Deze krijg je na het 'deployen' van het script in de Google Apps Script editor.
 //    Deze waarde wordt gebruikt als 'fallback' als er geen URL is ingesteld door de gebruiker in de app.
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzCE_6A08kA6ZzAAiJni0jDVgvvJ5tIQqek7Ayr7BHQmpnc5D0lMIZJVKpc6v15QO8_/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxNQftA6bBTUnww6kQjFJdjIHpj0et1bfK_Jz-88NPLQjHKk4elZG-fMWxhIvtBCk87/exec';
 
 // Fix: Add PLAYERS_SHEET_NAME export to resolve import error.
 // 3. De naam van het tabblad in je sheet waar de spelerslijst staat.
