d6cbf0730577a2a95e13ea1cdd35f49bb1c39a0d
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2025-12-03 08:14:47 +0100

    Update TrophyRoom.tsx

--- src/components/TrophyRoom.tsx
+++ src/components/TrophyRoom.tsx
@@ -90,9 +90,9 @@ const TrophyRoom: React.FC<TrophyRoomProps> = ({ trophies, players, isAuthentica
     if (type.includes('1ste') || type === 'Clubkampioen') return 'text-yellow-400';
     if (type.includes('2de')) return 'text-slate-600';
     if (type.includes('3de')) return 'text-amber-800';
-    if (type === 'Topscoorder') return 'text-yellow-300';
+    if (type === 'Topscoorder') return 'text-teal-300';
     if (type === 'Verdediger') return 'text-red-500';
-    if (type === 'Speler van het jaar') return 'text-white-500';
+    if (type === 'Speler van het jaar') return 'text-fuchsia-500';
     return 'text-white';
   };
 
