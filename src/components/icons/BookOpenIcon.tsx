746b6c962edb46941ba07faef71709962876d9b7
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2025-12-05 14:22:04 +0100

    Create BookOpenIcon.tsx

---
+++ src/components/icons/BookOpenIcon.tsx
@@ -0,0 +1,6 @@
+const BookOpenIcon = ({ className = "" }) => (
+  <svg className={className} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 19c-2.5-2-5-3-8-3V5c3 0 5.5 1 8 3m0 11c2.5-2 5-3 8-3V5c-3 0-5.5 1-8 3" />
+  </svg>
+);
+export default BookOpenIcon;
