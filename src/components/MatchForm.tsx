b39daaf4339c45532bbf58e5c4a3bc2c28a44676
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2025-12-10 20:32:11 +0100

    Update MatchForm.tsx

--- src/components/MatchForm.tsx
+++ src/components/MatchForm.tsx
@@ -187,12 +187,12 @@ const MatchForm: React.FC<MatchFormProps> = ({ teams, date }) => {
   // Eigen doelpunt (2 regels)
   blueContent = (
     <span className="text-gray-500 italic text-xs whitespace-pre-line">
-      {"Eigen goal team GEEL —\nNaam speler:"}
+      {"Eigen goal GEEL — Naam speler:"}
     </span>
   );
   yellowContent = (
     <span className="text-gray-500 italic text-xs whitespace-pre-line">
-      {"Eigen goal team BLAUW —\nNaam speler:"}
+      {"Eigen goal BLAUW — Naam speler:"}
     </span>
   );
 }
