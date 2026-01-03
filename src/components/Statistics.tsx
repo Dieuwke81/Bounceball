540dcf3ec8dc83901a36c7cabc723b8e7f95df0c
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2026-01-03 20:39:38 +0100

    Update Statistics.tsx

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,4 +1,5 @@
 
+
 import React, { useMemo, useState } from 'react';
 import type { GameSession, Player } from '../types';
 
@@ -531,7 +532,7 @@ const Statistics: React.FC<StatisticsProps> = ({ history, players, onSelectPlaye
 
       <div className="text-center mb-4">
         <p className="text-gray-400">
-          Statistieken gebaseerd op <span className="font-bold text-white">{totalSessions}</span> speeldagen. <span className="italic">Minimaal <span className="font-bold text-white">{minGames}</span speeldagen nodig.</span>
+          Statistieken gebaseerd op <span className="font-bold text-white">{totalSessions}</span> speeldagen. <span className="italic">Minimaal <span className="font-bold text-white">{minGames}</span> speeldagen nodig.</span>
         </p>
       </div>
 
